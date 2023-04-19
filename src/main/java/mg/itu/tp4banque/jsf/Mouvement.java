550de2d5a4ad7af3fbe0ff870e85d0be7077ee38
Tomiki0814 <tomikiramanantsoa@gmail.com>
2023-04-19 19:09:28 +0300

    correction retrait argent

--- src/main/java/mg/itu/tp4banque/jsf/Mouvement.java
+++ src/main/java/mg/itu/tp4banque/jsf/Mouvement.java
@@ -83,7 +83,7 @@ public class Mouvement implements Serializable {
       // Pour le cas où l'utilisateur a soumis le formulaire sans indiquer le type du mouvement
       return;
     }
-    if (valeurTypeMouvement.equals("****")) {
+    if (valeurTypeMouvement.equals("retrait")) {
       int retrait = (int) valeur;
       if (compte.getSolde() < retrait) {
         FacesMessage message
